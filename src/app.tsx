49613ecf3ea7bc68303fa753e1215571bd644d35
harri665 <harrison.d.martin@gmail.com>
2022-09-03 10:43:45 -0600

    added calander

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,23 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/joy/Box';
+import Typography from '@mui/joy/Typography';
+import Slider from '@mui/joy/Slider';
+import ReactDOM from 'react-dom/client';
+import MinimumDistanceSlider from './Componets/PersonDay';
+import { CssVarsProvider } from '@mui/joy/styles';
+import Calander from './Componets/Calander';
+
 
 function render() {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+  const root = ReactDOM.createRoot(document.querySelector("#root"))
+  // const [value, setValue] = React.useState<number[]>([20, 37]);
+  root.render(
+    <Calander/>
+  )
+  
 }
 
 render();
+
+
