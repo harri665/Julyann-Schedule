7520bc95a6b0b2e11c43648e5cbe5b9b96ef7511
harri665 <harrison.d.martin@gmail.com>
2022-11-03 17:09:14 -0600

    working

--- src/Componets/EditPersonWeek.tsx
+++ src/Componets/EditPersonWeek.tsx
@@ -15,8 +15,8 @@ import MenuItem from "@mui/joy/MenuItem";
 import Calendar from "./Calander/Calander";
 import Avatar from "@mui/joy/Avatar";
 import zIndex from "@mui/material/styles/zIndex";
-function test(value:number) {
-    return(12)
+function test(value: number) {
+  return 12;
 }
 function timetext(value: any) {
   let out = "";
@@ -34,8 +34,8 @@ export default function (props: any) {
   for (let x = 0; x < 24; x++) {
     times.push(x);
   }
-  var min = 5; 
-  var max =19; 
+  var min = 5;
+  var max = 19;
   //schedule
   // first nmae
   // last name
@@ -104,68 +104,103 @@ export default function (props: any) {
             </Grid>
           </Grid>
           {weekdays.map((value: any) => {
-            const valueLabelFormat = (x:number) => "Hi " + x;
-            return (
-              <Grid item xs={12 / 8} sx = {{position:"relative"}}>
-                <Slider
-                
-                  orientation="vertical"
-                  //   key={`slider-${slidervalue}`}
-                  //   onChangeCommitted={sliderchange}
-                  marks
-                  
-                  sx={{
-                    "--Slider-track-size": "34px",
-                    "--Slider-mark-size": "10px",
-                    "--Slider-mark-width": "34px",
-                    "--Slider-thumb-size": "28px",
-                    "--Slider-thumb-width": "34px",
-                    "--Slider-valueLabel-arrowSize": "-25px",
-                    width: "100%",
-                    height: "500px",
-                  }}
-                  min={-24}
-                  max={0}
-                  step={1}
-                  
-                  defaultValue={[-12, -14]}
-                  scale={x => -x}
-                  // key={JSON.stringify({ index: index })}
-                  getAriaValueText={timetext}
-                  valueLabelDisplay="on"
-                  valueLabelFormat = {valueLabelFormat}
-                />
-                <Box component="span" sx={{ 
-                        visibility: "visible",
-                        zIndex:1000000000000,
-                        position:"absolute", 
-                        top:0, 
-                        width:"100%", 
-                        backgroundColor: "black", 
-                        opacity: "70%", 
-                        
-                    }}
-                    height = {500*((min)/24)}
-                    >
-                </Box>
-                <Box component="span" sx={{ 
-                        visibility: "visible",
-                        zIndex:1000000000000,
-                        position:"absolute",  
-                        width:"100%", 
-                        backgroundColor: "black", 
-                        opacity: "50%", 
-                        
-                    }}
-                    height = {500*((min)/24)}
-                    top = {500-500*((24-max)/24)}
-                    >
-                </Box>
-              </Grid>
-            );
+            const valueLabelFormat = (x: number) => "Hi " + x;
+            return <PersonDay min={min} max={max} />;
           })}
         </Grid>
       </body>
     </Box>
+  );
+}
+
+function PersonDay(props: any) {
+  const [slidervalue, setslidervalue] = useState([-12, -14]);
+  const [textstart, settextstart] = useState("-12");
+  const [textend, settextend] = useState("-14");
+  function sliderchange(event: Event, valin: any) {
+    // console.log("HEY IT WORKS FOR REAL ", valin)
+    setslidervalue(valin);
+    settextstart(String(valin[0]));
+    settextend(String(valin[1]));
+  }
+  function textstartchange(event: Event, valin: number) {
+    settextstart(String(valin));
+    let temp: any = [parseInt(textstart), parseInt(textend)];
+    setslidervalue(temp);
+  }
+  function textendchange(event: Event, valin: number) {
+    settextend(String(valin));
+    let temp: any = [parseInt(textstart), parseInt(textend)];
+    setslidervalue(temp);
+  }
+
+  return (
+    <Grid item xs={12 / 8} sx={{ position: "relative" }}>
+      <Slider
+        orientation="vertical"
+        //   key={`slider-${slidervalue}`}
+        //   onChangeCommitted={sliderchange}
+        marks
+        sx={{
+          "--Slider-track-size": "34px",
+          "--Slider-mark-size": "10px",
+          "--Slider-mark-width": "34px",
+          "--Slider-thumb-size": "28px",
+          "--Slider-thumb-width": "34px",
+          "--Slider-valueLabel-arrowSize": "-25px",
+          width: "100%",
+          height: "500px",
+        }}
+        min={-24}
+        max={0}
+        step={1}
+        defaultValue={slidervalue}
+        scale={(x) => -x}
+        // key={JSON.stringify({ index: index })}
+        getAriaValueText={timetext}
+        valueLabelDisplay="on"
+        //   valueLabelFormat={valueLabelFormat}
+      />
+      <Box
+        component="span"
+        sx={{
+          visibility: "visible",
+          zIndex: 1000000000000,
+          position: "absolute",
+          top: 0,
+          width: "100%",
+          backgroundColor: "black",
+          opacity: "70%",
+        }}
+        height={500 * (props.min / 24)}
+      ></Box>
+      <Box
+        component="span"
+        sx={{
+          visibility: "visible",
+          zIndex: 1000000000000,
+          position: "absolute",
+          width: "100%",
+          backgroundColor: "black",
+          opacity: "50%",
+        }}
+        height={500 * (props.min / 24)}
+        top={500 - 500 * ((24 - props.max) / 24)}
+      ></Box>
+      <Input
+        // onChange={textstartchange}
+        type="number"
+        defaultValue={textstart}
+        key={JSON.stringify({ index: props.index })}
+        sx={{width:"50%"}}
+      ></Input>
+      <Input
+        // onChange={settextend}
+        type="number"
+        defaultValue={textend}
+        key={JSON.stringify({ index: props.index })}
+        sx={{width:"48%"}}
+      ></Input>
+    </Grid>
   );
 }
